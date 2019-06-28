f3926e0376e8162c6933760fc40491575f39530c
AzureAD\VivekMadan <vimadan2@in.ibm.com>
2019-06-28 13:23:48 +0530

    Code added for Player Search Functionality

--- CPlayersUI/src/app/modules/cpplayer/cpplayer.module.ts
+++ CPlayersUI/src/app/modules/cpplayer/cpplayer.module.ts
@@ -2,21 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponentComponent } from './components/card-component/card-component.component';
 import { HeaderComponentComponent } from './components/header-component/header-component.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatMenuModule} from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { AppRoutingModule } from '../../app-routing.module';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon';
-import {FormsModule} from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { CardContainerComponent } from './components/card-container/card-container.component';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
-  declarations: [CardComponentComponent, HeaderComponentComponent, RegisterComponent, LoginComponent, CardContainerComponent],
+  declarations: [CardComponentComponent, HeaderComponentComponent, RegisterComponent, LoginComponent, CardContainerComponent, DialogComponent],
   imports: [
     CommonModule,
     MatToolbarModule,
@@ -27,12 +29,14 @@ import { CardContainerComponent } from './components/card-container/card-contain
     MatIconModule,
     FormsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatTooltipModule
   ],
   exports: [
     CardComponentComponent,
     CardContainerComponent,
-    HeaderComponentComponent
+    HeaderComponentComponent,
+    MatTooltipModule
   ]
 })
 export class CpplayerModule { }
